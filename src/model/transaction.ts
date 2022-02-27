ff6083de08ab716031dc1e2109955017a35d3c3e
Igor <igorcbraz1@gmail.com>
2022-02-27 16:47:05 -0300

    Using prisma

---
+++ src/model/transaction.ts
@@ -0,0 +1,129 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function selectAllTransactions(id: number){
+    const allTransactions = await prisma.tbl_transactions.findMany({
+        where: {
+            FK_id_user: id
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+    if(allTransactions){
+        return allTransactions
+    } else {
+        return {
+            status: 'error'
+        }
+    }
+}
+
+interface InsertTransactionProps{
+    title: string;
+    tipo: string;
+    category: string;
+    amount: number;
+    payer: string;
+    createdAt: string;
+    FK_id_user: number;
+}
+export async function insertTransaction({
+    title,
+    tipo,
+    category,
+    amount,
+    payer,
+    createdAt,
+    FK_id_user
+}: InsertTransactionProps){
+    const insertTransaction = await prisma.tbl_transactions.create({
+        data: {
+            title,
+            tipo,
+            category,
+            amount,
+            payer,
+            createdAt,
+            FK_id_user
+        }
+    });
+
+    if(insertTransaction){
+        const lastTransaction = await prisma.tbl_transactions.findFirst({
+            orderBy: {
+                id: 'desc'
+            }
+        });
+
+        if(lastTransaction){
+            return lastTransaction;
+        } else {
+            return {
+                status: 'error'
+            }
+        }
+    } else {
+        return {
+            status: 'error'
+        }
+    }
+}
+
+export async function deleteTransaction(id: number, userId: number){
+    const deleteTransaction = await prisma.tbl_transactions.delete({
+        where: {
+            id: id
+        }
+    });
+
+    if(deleteTransaction){
+        const newTransactions = await selectAllTransactions(userId);
+
+        return newTransactions;
+    } else {
+        return {
+            status: 'error'
+        }
+    }
+}
+
+interface UpdateTransactionProps extends InsertTransactionProps{
+    id: number;
+}
+export async function updateTransaction({
+    title,
+    amount,
+    tipo,
+    category,
+    payer,
+    createdAt,
+    id,
+    FK_id_user
+}: UpdateTransactionProps){
+    const updateTransaction = await prisma.tbl_transactions.update({
+        where: {
+            id
+        }, 
+        data: {
+            title,
+            amount,
+            tipo,
+            category,
+            payer,
+            createdAt,
+        }
+    });
+
+    if(updateTransaction){
+        const newTransactions = await selectAllTransactions(FK_id_user);
+
+        return newTransactions;
+    } else {
+        return {
+            status: 'error'
+        }
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,20 @@
 import { Router } from "express";
 import { UserController } from "./controller/User";
+import { TransactionController } from "./controller/Transactions";
 
 const router = Router();
 
-//User
 const user = new UserController();
+const transactions = new TransactionController();
 
 // Transactions routes
-router.get('/transactions/:id',)
-router.post('/transactions',)
-router.delete('/transactions/:id/:userId')
-router.put('/transactions',)
+router.get('/transactions/:id'           , transactions.select)
+router.post('/transactions'              , transactions.insert)
+router.delete('/transactions/:id/:userId', transactions.delete)
+router.put('/transactions'               , transactions.update)
 
 // User routes
-router.post('/login', user.login)
+router.post('/login'   , user.login)
 router.post('/register', user.register)
 
 export { router };
